60a924ed3893c5391b416b439fe060c331ae1152
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-05-11 09:15:29 +0300

    add image

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,56 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvater1
+  /// assets/images/avater1.svg
+  static const String imagesAvatar1 = "assets/images/avater1.svg";
+
+  /// Assets for imagesAvater2
+  /// assets/images/avater2.svg
+  static const String imagesAvatar2 = "assets/images/avater2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCart
+  /// assets/images/cart.svg
+  static const String imagesCart = "assets/images/cart.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMyInvestments
+  /// assets/images/myInvestments.svg
+  static const String imagesMyInvestments = "assets/images/myInvestments.svg";
+
+  /// Assets for imagesMyTrsndaction
+  /// assets/images/myTrsndaction.svg
+  static const String imagesMyTrsndaction = "assets/images/myTrsndaction.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesWallet
+  /// assets/images/wallet.svg
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
+
