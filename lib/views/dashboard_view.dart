822956eff889a09048895dd7254793e6eff9bd91
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-05-11 08:24:56 +0300

    Add responsive dashboard view and related widgets
    Introduce a responsive dashboard view with adaptive layout for different screen
    sizes. Also, include desktop layout widget for the dashboard view.

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
+  }
+}
