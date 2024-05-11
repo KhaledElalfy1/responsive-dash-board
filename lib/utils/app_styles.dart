dc2ad3833ff53cfc96382f5f7f3b30489aab6bec
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-05-11 09:19:29 +0300

    add font family

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,54 +1,66 @@
 import 'package:flutter/material.dart';
 
-abstract class AppStyles{
-  static const TextStyle styleRegular16=TextStyle(
+abstract class AppStyles {
+  static const String _fontFamily = 'Montserrat';
+
+  static const TextStyle styleRegular16 = TextStyle(
     color: Color(0xff064061),
     fontSize: 16,
     fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
   );
-  static const TextStyle styleMedium16=TextStyle(
+  static const TextStyle styleMedium16 = TextStyle(
     color: Color(0xff064061),
     fontSize: 16,
     fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
   );
-  static const TextStyle styleSemiBold16=TextStyle(
+  static const TextStyle styleSemiBold16 = TextStyle(
     color: Color(0xff064061),
     fontSize: 16,
     fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
   );
-  static const TextStyle styleBold16=TextStyle(
+  static const TextStyle styleBold16 = TextStyle(
     color: Color(0xff4EB7F2),
     fontSize: 16,
     fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
   );
-  static const TextStyle styleSemiBold20=TextStyle(
+  static const TextStyle styleSemiBold20 = TextStyle(
     color: Color(0xff064061),
     fontSize: 20,
     fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
   );
-  static const TextStyle styleRegular12=TextStyle(
+  static const TextStyle styleRegular12 = TextStyle(
     color: Color(0xffAAAAAA),
     fontSize: 12,
     fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
   );
-  static const TextStyle styleSemiBold24=TextStyle(
+  static const TextStyle styleSemiBold24 = TextStyle(
     color: Color(0xff4EB7F2),
     fontSize: 24,
     fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
   );
-  static const TextStyle styleRegular14=TextStyle(
+  static const TextStyle styleRegular14 = TextStyle(
     color: Color(0xffAAAAAA),
     fontSize: 14,
     fontWeight: FontWeight.w400,
+    fontFamily: _fontFamily,
   );
-   static const TextStyle styleSemiBold18=TextStyle(
+  static const TextStyle styleSemiBold18 = TextStyle(
     color: Color(0xff4EB7F2),
     fontSize: 18,
     fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
   );
-   static const TextStyle styleMedium20=TextStyle(
+  static const TextStyle styleMedium20 = TextStyle(
     color: Color(0xffFFFFFF),
     fontSize: 20,
     fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
   );
 }
