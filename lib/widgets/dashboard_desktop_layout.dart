d736099a6f80fc76e0c90c4fcf5bef9dd3ec4f0d
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-05-12 06:50:23 +0300

    Add new AllExpenses widget and update DashboardView layout
    - Added a new AllExpenses widget with header
    - Updated DashboardView layout to include AllExpenses widget for desktop view

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:responsive_dash_board/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -12,6 +14,15 @@ class DashboardDesktopLayout extends StatelessWidget {
         Expanded(
           child: CustomDrawer(),
         ),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+          flex: 2,
+          child: Column(
+            children: [Expanded(child: AllExpenses())],
+          ),
+        )
       ],
     );
   }
